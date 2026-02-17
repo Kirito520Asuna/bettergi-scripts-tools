0e52cb81df4ee3e1d47bddec09dee455c60329e4
yan <doutianmianxia@qq.com>
2026-02-18 00:54:36 +0800

    refactor(domain): 提取AutoFight和AutoLeyLineOutcrop为独立域对象
    - 将AutoFight类从内部静态类提取为独立的域对象
    - 将AutoLeyLineOutcrop类从内部静态类提取为独立的域对象
    - 将Physical类提取为独立的域对象
    - 在AutoPlanDTO中引入对新域对象的引用
    - 在AutoPlanVo中引入对新域对象的引用
    - 移除原有的内部静态类定义
    - 保持所有字段和注解的完整性

--- bgi-tools/src/main/java/com/cloud_guest/domain/dto/AutoPlanDTO.java
+++ bgi-tools/src/main/java/com/cloud_guest/domain/dto/AutoPlanDTO.java
@@ -1,5 +1,7 @@
 package com.cloud_guest.domain.dto;
 
+import com.cloud_guest.domain.AutoFight;
+import com.cloud_guest.domain.AutoLeyLineOutcrop;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -39,84 +41,4 @@ public class AutoPlanDTO implements Serializable {
     @Schema(description = "地脉参数")
     @JsonProperty("autoLeyLineOutcrop")
     private AutoLeyLineOutcrop autoLeyLineOutcrop;
-
-    @NoArgsConstructor
-    @Data
-    public static class AutoLeyLineOutcrop {
-        // 刷取次数
-        @Schema(description = "刷取次数")
-        public int count;
-        @Schema(description = "国家地区")
-        public String country;
-        @Schema(description = "地脉花类型 ")
-        //地脉花类型
-        public String leyLineOutcropType;
-        //@Schema(description = "是否开启树脂耗尽模式")
-        //@JsonProperty("isResinExhaustionMode")
-        //// 是否开启树脂耗尽模式
-        //public boolean isResinExhaustionMode;
-        //@Schema(description = "[耗尽模式]是否开启取小值模式")
-        //// 开启取小值模式
-        //public boolean openModeCountMin;
-        @Schema(description = "是否使用冒险之证寻找地脉花")
-        //是否使用冒险之证寻找地脉花
-        public boolean useAdventurerHandbook;
-        @Schema(description = "好感队名称")
-        //好感队名称
-        public String friendshipTeam;
-        @Schema(description = "战斗的队伍名称")
-        //战斗的队伍名称
-        public String team;
-        @Schema(description = "战斗超时时间")
-        //战斗超时时间
-        public int timeout = 120;
-        @Schema(description = "是否前往合成台合成浓缩树脂")
-        @JsonProperty("isGoToSynthesizer")
-        //是否前往合成台合成浓缩树脂
-        public boolean isGoToSynthesizer;
-        @Schema(description = "是否使用脆弱树脂")
-        //是否使用脆弱树脂
-        public boolean useFragileResin;
-        @Schema(description = "是否使用须臾树脂")
-        //是否使用须臾树脂
-        public boolean useTransientResin;
-        @Schema(description = "通过BGI通知系统发送详细通知")
-        @JsonProperty("isNotification")
-        //通过BGI通知系统发送详细通知
-        public boolean isNotification;
-    }
-
-    @NoArgsConstructor
-    @Data
-    public static class AutoFight {
-        //@Schema(description = "国家地区")
-        //@JsonProperty("country")
-        //public String country;
-        @Schema(description = "秘境名称")
-        @JsonProperty("domainName")
-        private String domainName;
-        @Schema(description = "限时/周日 顺序1-3")
-        @JsonProperty("sundaySelectedValue")
-        private Integer sundaySelectedValue;
-        @Schema(description = "队伍")
-        @JsonProperty("partyName")
-        private String partyName;
-        @Schema(description = "秘境轮数")
-        @JsonProperty("domainRoundNum")
-        private Integer domainRoundNum;
-        @Schema(description = "树脂启用顺序")
-        @JsonProperty("physical")
-        private List<Physical> physical;
-    }
-
-    @NoArgsConstructor
-    @Data
-    public static class Physical {
-        @JsonProperty("order")
-        private Integer order;
-        @JsonProperty("name")
-        private String name;
-        @JsonProperty("open")
-        private boolean open;
-    }
 }
