dba3e77f5e2d3d3cd8487f80c7295f8adba7109d
yan <doutianmianxia@qq.com>
2026-02-23 16:59:55 +0800

    feat(auth): 添加授权token保存功能
    - 在ApplicationService接口中新增saveToken方法并添加SneakyThrows注解
    - 实现ApplicationServiceImpl类中的saveToken方法，支持YAML文件token写入
    - 集成YmlUtils和FileUtil工具类进行YAML文件读写操作
    - 在AuthTokenController中添加POST接口用于设置授权token信息
    - 创建CheckTokenDto数据传输对象用于token名称和值的验证
    - 添加表单验证注解确保token名称和值不为空
    - 实现多YAML路径遍历和token信息更新逻辑

--- bgi-tools/src/main/java/com/cloud_guest/service/ApplicationService.java
+++ bgi-tools/src/main/java/com/cloud_guest/service/ApplicationService.java
@@ -1,9 +1,13 @@
 package com.cloud_guest.service;
 
+import lombok.SneakyThrows;
+
 /**
  * @Author yan
  * @Date 2026/2/23 16:11:19
  * @Description
  */
 public interface ApplicationService {
+    @SneakyThrows
+    boolean saveToken(String name, String value);
 }
--- bgi-tools/src/main/java/com/cloud_guest/service/impl/ApplicationServiceImpl.java
+++ bgi-tools/src/main/java/com/cloud_guest/service/impl/ApplicationServiceImpl.java
@@ -1,7 +1,16 @@
 package com.cloud_guest.service.impl;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.json.JSONObject;
+import com.cloud_guest.properties.load.LoadProperties;
 import com.cloud_guest.service.ApplicationService;
+import com.cloud_guest.utils.yml.YmlUtils;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.List;
 
 /**
  * @Author yan
@@ -10,4 +19,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
+
+    @Resource
+    private LoadProperties loadProperties;
+
+    @SneakyThrows
+    @Override
+    public boolean saveToken(String name, String value) {
+        List<String> yamlPaths = loadProperties.getYamlPaths();
+        for (String yamlPath : yamlPaths) {
+            try {
+                File file = FileUtil.newFile(yamlPath);
+                JSONObject jsonObject = YmlUtils.readValue(file, JSONObject.class);
+                JSONObject byPath = (JSONObject) jsonObject.getByPath("check.token");
+                if (byPath == null) {
+                    byPath = new JSONObject();
+                }
+                byPath.put("name", name);
+                byPath.put("value", value);
+                YmlUtils.writeValue(file, jsonObject);
+            } catch (Exception e) {
+                if (e.getMessage().contains("文件不存在或为空")) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
